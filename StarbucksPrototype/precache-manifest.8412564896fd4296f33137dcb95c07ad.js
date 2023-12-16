94161cd4754004f1e8dc72997cb9b4e00a9e7d7e
tenzinsherap <tenzin.nepali2@marist.edu>
2023-12-16 14:04:50 -0500

    Starbucks Prototype Website

---
+++ StarbucksPrototype/precache-manifest.8412564896fd4296f33137dcb95c07ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5532f3d8d1cda90e3acf1cf6703b13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e6771988de73a6a60e3",
+    "url": "./static/css/main.1fede130.chunk.css"
+  },
+  {
+    "revision": "2c323a1a023375f32de6",
+    "url": "./static/js/2.13358188.chunk.js"
+  },
+  {
+    "revision": "5e6771988de73a6a60e3",
+    "url": "./static/js/main.812fbdd1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
